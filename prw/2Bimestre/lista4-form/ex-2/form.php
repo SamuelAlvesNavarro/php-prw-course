a2dc0096da73f0b7338b429a1364af47b1dd0d23
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-04-28 19:02:58 -0300

    php

--- prw/2Bimestre/lista4-form/ex-2/form.php
+++ prw/2Bimestre/lista4-form/ex-2/form.php
@@ -5,5 +5,17 @@
     
     $comentario = $_POST['comentario'];
     $genero = $_POST['genero'];
+
+    $cores = $_POST['cor'];
+    $esporte = $_POST['esporte'];
+
+    echo "Bem vindo, $nome, o gênero inserido foi $genero<br>";
+    echo "Seu email inserido é: $email. E sua senha inserida é $senha<br>";
+    echo "Comentário inserido: $comentario<br>";
+    echo "Cor(es):<br>";
+    foreach($cores as $key => $value){
+        echo ++$key . " $value<br>";
+    }
+    echo "Seu esporte escolhido é: $esporte<br>";
     
 ?>
