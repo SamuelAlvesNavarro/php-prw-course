80d647e85433f99cc8aeec5aca60569800e78c45
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-04-30 18:25:28 -0300

    php

--- prw/2Bimestre/lista4-form/ex-6/form.php
+++ prw/2Bimestre/lista4-form/ex-6/form.php
@@ -3,4 +3,15 @@
     $n1 = $_POST['n1'];
     $n2 = $_POST['n2'];
     
+    $m = ($n1 + $n2) / 2;
+
+    if($frequencia < 75){
+        echo "Reprovado por <strong>frequência</strong>";
+    } else{
+        if($m < 5){
+            echo "Reprovado por <strong>nota</strong>";
+        }else{
+            echo "<strong>Aprovado</strong>";
+        }
+    }
 ?>
