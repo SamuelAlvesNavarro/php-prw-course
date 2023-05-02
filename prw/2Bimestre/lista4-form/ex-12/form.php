33185e4b53d256ce8916dd929a50150c68084fb4
SamuelAlvesNavarro <samuelalvesnavarro1@gmail.com>
2023-05-02 07:41:13 -0300

    php

--- prw/2Bimestre/lista4-form/ex-12/form.php
+++ prw/2Bimestre/lista4-form/ex-12/form.php
@@ -1,16 +1,16 @@
 <?php
 
-$pasta = $_POST['pasta'];
-$num = $_POST['numero'];
+    $pasta = $_POST['pasta'];
+    $num = $_POST['numero'];
 
-echo "<table>";
+    echo "<table>";
 
-for($x = 1; $x <= $num; $x++){
-    echo "<tr>";
-    echo "<td><img src='$pasta/$x.jpg' width='150' height='100'></td>";
-    echo "</tr>";
-}
+    for($x = 1; $x <= $num; $x++){
+        echo "<tr>";
+        echo "<td><img src='$pasta/$x.jpg' width='150' height='100'></td>";
+        echo "</tr>";
+    }
 
-echo "</table>";
+    echo "</table>";
 
 ?>
